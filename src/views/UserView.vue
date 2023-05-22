1fb011e5093145136e5b0ed905e3146f74b80dbf
Rudy Artha <rudy.artha@guestpro.id>
2023-05-22 22:52:39 +0800

    add call api example

---
+++ src/views/UserView.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <div class="green">
+            <li v-for="(user, index) in users" v-bind:key="index">{{user.username}}
+            </li>            
+        </div>
+
+    </main>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            users: null
+        };
+    },
+    methods: {
+        findUsers() {
+            axios.get('http://localhost:8080/api/users')
+            .then((response) => {
+                this.users = response.data.data;
+                // console.log(this.users);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+        }
+    },
+    mounted() {
+        this.findUsers();
+    },
+};
+</script>
